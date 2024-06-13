27dcc30686ba2f81c1f8005b35edebdcf3d9792c
Álvaro Gascón <alvarogascon@hotmail.com>
2024-06-13 09:16:36 +0200

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Álvaro Gascón on 13/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
